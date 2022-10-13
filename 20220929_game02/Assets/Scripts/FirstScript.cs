1f004e9af926d952abcdb88b6510f5c8fe9c9d6e
candy-hsu <62374659+candy-hsu@users.noreply.github.com>
2022-10-13 15:53:50 +0800

    20221013-5

--- 20220929_game02/Assets/Scripts/FirstScript.cs
+++ 20220929_game02/Assets/Scripts/FirstScript.cs
@@ -1,7 +1,19 @@
 
 using UnityEngine;
-
-public class FirstScript : MonoBehaviour
+namespace Uzai
 {
-    
+    /// <summary>
+    /// 第一次的C++
+    /// </summary>
+    public class FirstScript : MonoBehaviour
+    {
+        #region 註解
+        // 單行註解
+        /* 多行註解
+         * 
+         */
+        //A a 不同字
+        #endregion
+    }
 }
+

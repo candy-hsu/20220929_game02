5fa2e6f9a81f77f3cf88deb8f28253265fa85d38
candy-hsu <62374659+candy-hsu@users.noreply.github.com>
2022-10-20 14:43:52 +0800

    20221020-3

--- 20220929_game02/Assets/Scripts/LearnField.cs
+++ 20220929_game02/Assets/Scripts/LearnField.cs
@@ -6,10 +6,20 @@ namespace Candy
     /// 欄位的練習
     /// </summary>
 
-    public class LearnFile : MonoBehaviour
+    public class LearnField : MonoBehaviour
     {
-        #region 欄位
-
+        #region 欄位 語法 private
+        //欄位 Field 儲存資料
+        //語法：
+        //private 修飾詞 私人 僅在此類別內能夠儲存,不顯示
+        //int 資料類型 整數
+        //level 欄位名稱 可更改該名稱
+        private int Level;
+        #endregion
+        #region 欄位 語法 public
+        //等於 指定符號 將右邊的結果指定給左邊
+        //public 公開 所有類別都能夠存取,顯示
+        public int Enemy = 100;
         #endregion
     }
 

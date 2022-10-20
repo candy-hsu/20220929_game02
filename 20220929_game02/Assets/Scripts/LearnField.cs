d229abd488edd58a506bac0d1b66d01f73f1c5fd
candy-hsu <62374659+candy-hsu@users.noreply.github.com>
2022-10-20 15:15:59 +0800

    20221020-6

--- 20220929_game02/Assets/Scripts/LearnField.cs
+++ 20220929_game02/Assets/Scripts/LearnField.cs
@@ -16,10 +16,27 @@ namespace Candy
         //level 欄位名稱 可更改該名稱
         private int Level;
         #endregion
-        #region 欄位 語法 public
+        #region 欄位 語法 public 資料類型
         //等於 指定符號 將右邊的結果指定給左邊
         //public 公開 所有類別都能夠存取,顯示
+        //int 整數
         public int Enemy = 100;
+        //string 文字
+        public string nameplayer = "美女";
+        //float 小數點 後加f
+        public float Speed = 5.5f;
+        public float Jump = 7.8f;
+        //bool 布林值 false true
+        public bool isPass = false;
+        public bool hasWeapon = true;
+        #endregion
+        #region 欄位屬性
+        //Header 標題
+        [Header("血量")]
+        public int hp = 5;
+        //Tooltip 提示
+        [Tooltip("玩家打敗敵人的時間限制。")]
+        public float Time = 8.8f;
         #endregion
     }
 

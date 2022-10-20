07d4b00642549cf24c6d5fef3bd1c9632240c455
candy-hsu <62374659+candy-hsu@users.noreply.github.com>
2022-10-20 16:08:35 +0800

    20221020-10

---
+++ 20220929_game02/Assets/Scripts/AirPlaneController2D.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+namespace Uzai
+{
+    /// <summary>
+    /// 飛機控制器2D模式
+    /// </summary>
+public class AirPlaneController2D : MonoBehaviour
+{
+        [Header("移動速度")]
+        [SerializeField, Range(0, 10)]
+        private float SpeedVeatical = 3.5f;
+        [SerializeField, Range(0, 10)]
+        private float SpeedHorizontal = 4;
+
+        [Header("圖片")]
+        [SerializeField]
+        private Sprite PictureUp;
+        [SerializeField]
+        private Sprite PictureMiddle;
+        [SerializeField]
+        private Sprite PictureDown;
+
+
+
+}
+
+}
+

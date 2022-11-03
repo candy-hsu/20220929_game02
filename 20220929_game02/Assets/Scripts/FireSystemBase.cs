8905d307b3bd71ea4b67cabf96115a90d777a2ba
candy-hsu <62374659+candy-hsu@users.noreply.github.com>
2022-11-03 15:55:52 +0800

    20221103-1

--- 20220929_game02/Assets/Scripts/FireSystemBase.cs
+++ 20220929_game02/Assets/Scripts/FireSystemBase.cs
@@ -12,6 +12,16 @@ namespace Uzai
         private GameObject prefabBullet;
         [SerializeField, Header("子彈生成點")]
         private Transform pointSpawn;
+
+        //自訂方法
+        //生成子彈
+        //protected 保護級別：允許子類別存取
+        protected void SpawnBullet()
+        {
+            //實例化(物件.座標.角度)
+            //生成 子彈預製物 角度與座標根子彈生成點相同
+            Instantiate(prefabBullet, pointSpawn.position, pointSpawn.rotation);
+        }
     }
 }
 
--- 20220929_game02/Assets/Scripts/PlayerFireSytem.cs
+++ 20220929_game02/Assets/Scripts/PlayerFireSytem.cs
@@ -7,7 +7,15 @@ namespace Uzai
     /// </summary>
     public class PlayerFireSytem : FireSystemBase
     {
-
+        //偵測玩家輸入行為：鍵盤、滑鼠、搖桿
+        private void Update()
+        {
+            //如果 玩家 按下空白鍵 就生成子彈
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SpawnBullet();
+            }
+        }
     }
 }
 
